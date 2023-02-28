44b0e709002226105d6934f6a564ddabdd222c34
Skydevilk <Skydevilk@gmail.com>
2023-02-28 18:50:06 +0900

    Test용 프로젝트

---
+++ BatchScheduler_Spring/src/main/java/com/example/batchscheduler_spring/BatchSchedulerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchscheduler_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchSchedulerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchSchedulerSpringApplication.class, args);
+    }
+
+}
---
+++ BatchScheduler_Spring/src/test/java/com/example/batchscheduler_spring/BatchSchedulerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchscheduler_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchSchedulerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
